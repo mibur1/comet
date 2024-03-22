cc552d0cad064363812af2d57f0dbe2240af637a
Micha Burkhardt <micha.burkhardt@uni-oldenburg.de>
2024-03-22 15:09:27 +0100

    support for .tsv data

--- src/comet/data.py
+++ src/comet/data.py
@@ -3,11 +3,64 @@ import re
 import sys
 import pickle
 import numpy as np
+import pandas as pd
 from nilearn import signal
+from scipy.io import loadmat
 import importlib.resources as pkg_resources
 
 from .methods import *
 from .multiverse import in_notebook
+
+def load_timeseries(path=None, rois=None):
+    """
+    Load time series data from a file
+    """
+    if path is None:
+        raise ValueError("Please provide a path to the time series data")
+    
+    if path.endswith(".pkl"):
+        with open(path, 'rb') as file:
+            data = pickle.load(file)
+    elif path.endswith(".txt"):
+        data = np.loadtxt(path)
+    elif path.endswith(".npy"):
+        data = np.load(path)
+    elif path.endswith(".mat"):
+        data = loadmat(path)
+    elif path.endswith(".tsv"):
+        data = pd.read_csv(path, sep='\t', header=None, na_values='n/a')
+
+        if data.iloc[0].apply(lambda x: np.isscalar(x) and np.isreal(x)).all():
+            rois = None  # No rois found, the first row is part of the data
+        else:
+            rois = data.iloc[0]  # The first row is rois
+            data = data.iloc[1:]  # Remove the header row from the data
+
+        # Convert all data to numeric, making sure 'n/a' and other non-numeric are treated as NaN
+        data = data.apply(pd.to_numeric, errors='coerce')
+
+        # Identify entirely empty columns
+        empty_columns = data.columns[data.isna().all()]
+        
+        # Remove corresponding rois if rois exist
+        if rois is not None:
+            removed_rois = rois[empty_columns].to_list()
+            print("The following regions were empty and thus removed:", removed_rois)
+            rois = rois.drop(empty_columns)
+
+        # Remove entirely empty columns and rows
+        data = data.dropna(axis=1, how='all').dropna(axis=0, how='all')
+
+        # Convert the cleaned data back to numpy array
+        data = data.to_numpy()
+
+        # Update header_list if rois exist
+        rois = rois.to_list() if rois is not None else None
+
+    else:
+        raise ValueError("Unsupported file format")
+    
+    return data, rois
 
 def load_example(type=None):
     """
