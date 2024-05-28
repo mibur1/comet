9ff236a4a847b0151ce5bb082e2148bb4b810ae2
Micha Burkhardt <micha.burkhardt@uni-oldenburg.de>
2024-05-28 13:52:13 +0200

    clean up

--- tutorials/example_multiverse/universe_23.py
+++ tutorials/example_multiverse/universe_23.py
@@ -5,12 +5,12 @@ import comet
 print(f"Decision 1: 'world'")
 print(f"Decision 2: 2")
 print(f"Decision 3:False")
-    
+
 # Load example data and calculate dFC + local efficiency
 ts = comet.data.load_example()
 dfc = comet.methods.LeiDA(ts, **{}).connectivity()
 dfc = dfc[0] if isinstance(dfc, tuple) else dfc #required as LeiDA returns multiple outputs
-    
+
 efficiency = np.zeros((ts.shape[0], dfc.shape[1]))
 for i in range(dfc.shape[2]):
     W = dfc[:, :, i]
