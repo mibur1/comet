ac34b94fb1a9b756e247defe5b2a265636936bef
Micha Burkhardt <micha.burkhardt@gmx.de>
2025-09-16 14:53:09 +0200

    ignore NaN warnings in centrality calculations

--- src/comet/graph.py
+++ src/comet/graph.py
@@ -5,6 +5,10 @@ import scipy.sparse
 from numba import jit
 from typing import Literal
 
+# Ignore warnings from bctpy about NaNs in centrality calculations
+warnings.filterwarnings("ignore", message=r"invalid value encountered in divide",
+                        category=RuntimeWarning, module=r".*bct\.algorithms\.centrality")
+
 """
 SECTION: Graph processing functions
  - General functions to preprocess connectivity/adjacency matrices
